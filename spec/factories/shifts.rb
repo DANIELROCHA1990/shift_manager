93373b1c28bb9279aedd50c6a188cd5fd20d81f8
Daniel_Rocha <daniel1990rocha@gmail.com>
2022-05-25 12:19:26 -0300

    Creates test shift archives

---
+++ spec/factories/shifts.rb
@@ -0,0 +1,8 @@
+FactoryBot.define do
+  factory :shift do
+    current_day { "2022-05-24 18:39:36" }
+    clock_in { "MyString" }
+    clock_out { "MyString" }
+    all_clock { "MyString" }
+  end
+end
