28d7bc909e65ba3fbe2b5de3923458ba04de9959
Daniel_Rocha <daniel1990rocha@gmail.com>
2022-05-25 12:22:16 -0300

    Set some sonfigs and del email directory view

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_05_16_180145) do
+ActiveRecord::Schema[7.0].define(version: 2022_05_25_141503) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "shifts", force: :cascade do |t|
+    t.datetime "current_day"
+    t.string "clock_in"
+    t.string "clock_out"
+    t.string "all_clock"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_shifts_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -26,4 +37,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_05_16_180145) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "shifts", "users"
 end
