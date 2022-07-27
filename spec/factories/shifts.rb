2b526d027cfd92f7d6d6ac018c45c55c89be5839
Daniel_Rocha <daniel1990rocha@gmail.com>
2022-07-27 09:46:13 -0300

    Create shifts tests

--- spec/factories/shifts.rb
+++ spec/factories/shifts.rb
@@ -1,8 +1,7 @@
 FactoryBot.define do
   factory :shift do
     current_day { "2022-05-24 18:39:36" }
-    clock_in { "MyString" }
-    clock_out { "MyString" }
-    all_clock { "MyString" }
+    clock_in { DateTime.now.strftime("%d/%m/%y  %H:%m") }
+    clock_out { DateTime.now.strftime("%d/%m/%y  %H:%m") }
   end
 end
