2c0e7cc608024e9042e3e1e373421848bc45d8af
Daniel_Rocha <daniel1990rocha@gmail.com>
2022-07-28 08:49:34 -0300

    Config views

--- app/models/shift.rb
+++ app/models/shift.rb
@@ -4,12 +4,12 @@ class Shift < ApplicationRecord
   before_create :set_clock_in
 
   def set_clock_out
-    self.clock_out = DateTime.now
+    self.clock_out = DateTime.now.strftime('%H:%M:%S')
   end
 
   private
 
   def set_clock_in
-    self.clock_in = DateTime.now
+    self.clock_in = DateTime.now.strftime('%H:%M:%S')
   end
 end
