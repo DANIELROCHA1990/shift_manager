e128053d5117f87b724c4c8b46d92f60a60115ab
Daniel_Rocha <daniel1990rocha@gmail.com>
2022-07-28 19:53:40 -0300

    Fix missconfig

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,26 +1,33 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  let(:valid_user) { build(:user, :valid_attrib) }
-  let(:no_user_email) { build(:user, :no_email) }
-  let(:no_user_password) { build(:user, :no_password) }
-  let(:no_user_user_name) { build(:user, :no_user_name) }
+  subject { build(:user, attributes) }
+  let(:attributes) { { email: 'user@user.com', password: 'teste123', user_name: 'user' } }
 
   context 'Succes to create' do
     it 'user because is valid' do
-      expect(valid_user).to be_valid
+      expect(subject).to be_valid
     end
   end
 
   context 'Failure to create' do
     it 'user without email' do
-      expect(no_user_email).not_to be_valid
+      attributes[:email] = nil
+      expect(subject).not_to be_valid
     end
+
+    # it 'user with email duplicated' do
+    #   attributes[:email] = nil
+    #   expect(subject).not_to be_valid
+    # end
+
     it 'user without password' do
-      expect(no_user_password).not_to be_valid
+      attributes[:password] = nil
+      expect(subject).not_to be_valid
     end
     it 'user without user_name' do
-      expect(no_user_user_name).not_to be_valid
+      attributes[:user_name] = nil
+      expect(subject).not_to be_valid
     end
   end
 end
