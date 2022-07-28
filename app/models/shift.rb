65427edba320b252d6556072cab2cf66d586a815
Daniel_Rocha <daniel1990rocha@gmail.com>
2022-07-28 19:51:09 -0300

    Adjust methods

--- app/models/shift.rb
+++ app/models/shift.rb
@@ -4,12 +4,12 @@ class Shift < ApplicationRecord
   before_create :set_clock_in
 
   def set_clock_out
-    self.clock_out = DateTime.now.strftime('%H:%M:%S')
+    self.clock_out = DateTime.now
   end
 
   private
 
   def set_clock_in
-    self.clock_in = DateTime.now.strftime('%H:%M:%S')
+    self.clock_in = DateTime.now
   end
 end
