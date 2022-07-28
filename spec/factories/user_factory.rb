7bf9bbd4e3be87a9a5e06ecb1248efc9ab50f311
Daniel_Rocha <daniel1990rocha@gmail.com>
2022-07-28 19:53:11 -0300

    Adjust trait

--- spec/factories/user_factory.rb
+++ spec/factories/user_factory.rb
@@ -1,27 +1,9 @@
 FactoryBot.define do
   factory :user do
-    trait :valid_attrib do
+    trait :attributes do
       email { 'user@user.com' }
       password { 'teste123' }
       user_name { 'user' }
-    end
-
-    trait :no_email do
-      email { nil }
-      password { 'teste123' }
-      user_name { 'user' }
-    end
-
-    trait :no_password do
-      email { 'user@user.com' }
-      password { nil }
-      user_name { 'user' }
-    end
-
-    trait :no_user_name do
-      email { 'user@user.com' }
-      password { 'teste123' }
-      user_name { nil }
     end
   end
 end
