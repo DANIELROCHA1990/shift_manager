670278348d219bb56b6fe49547de1147192a7f25
Rafael Hashimoto <hashimotorafa@gmail.com>
2022-10-21 19:37:16 -0300

    refactor routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,16 +2,20 @@
 
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'user/shifts#index'
 
-  post '/users/:user_id/shifts/:id/create_description' => 'user/shifts#create_description',
-       as: 'create_user_description'
-  get '/users/:user_id/shifts/:id' => 'user/shifts#new_description', as: 'new_user_description'
-  get '/users/:user_id/shifts/:id' => 'user/shifts#show_description', as: 'show_user_description'
+  as: 'create_user_description'
 
   namespace :user do
+    root to: 'user/shifts#index'
+
     resources :users do
-      resources :shifts
+      resources :shifts do
+        member do
+          post 'create_description'
+          get  'show_user_description'
+          get  'new_user_description'
+        end
+      end
     end
     resources :shifts
   end
